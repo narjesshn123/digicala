87881646f1424b3add0ceb9bd8e8bb8a55752fcf
digicala <narjessheikhali@gmail.com>
2022-08-02 23:44:19 +0430

    mousedropdown

--- digicala/src/components/Nav/MenuItem.jsx
+++ digicala/src/components/Nav/MenuItem.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { Dropdown } from './Dropdown'
 
 const MenuItem = ({menu, depthLevel}) => {
   const[dropdown, setDropdown] = useState(false)
+  let ref = useRef()
+  const onMouseEnter = ()=>{
+    window.innerWidth > 960 && setDropdown(true)
+  }
+  const onMouseLeave = ()=>{
+    window.innerWidth > 960 && setDropdown(false)
+  }
+  useEffect(()=>{
+    const handler = (event)=>{
+      if(dropdown && ref.current && !ref.current.contain(event.target)){
+        setDropdown(false)
+      }
+    }
+    document.addEventListener("mousedown", handler)
+    document.addEventListener("touchstart", handler)
+    return()=>{
+      document.removeEventListener("mousedown", handler)
+      document.removeEventListener("touchstart", handler)
+    }
+  },[dropdown])
   return (
-    <li className="menu-items">
+    <li className="menu-items" ref= {ref}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+    >
     {menu.submenu ? (
      <>
       <button aria-expanded={dropdown ? "true" : "false"} 
