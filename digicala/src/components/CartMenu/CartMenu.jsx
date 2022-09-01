d8b0b5056a5dde86a444254378258e76fbc4773f
digicala <narjessheikhali@gmail.com>
2022-09-02 01:20:01 +0430

    add to cart and decrease add to cartmenu

--- digicala/src/components/CartMenu/CartMenu.jsx
+++ digicala/src/components/CartMenu/CartMenu.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import styles from './CartMenu.style.module.css'
 import cini from '../../assets/image/cini.jpg'
 import CartMenuItem from '../CartMenuItem/CartMenuItem'
-import { BiTrash } from 'react-icons/bi'
+import { BiTrash, BiPlus, BiMinus } from 'react-icons/bi'
 import {Link} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { useMemo } from 'react'
+import { addtoCart, remove, decrease } from '../../redux/reducers/Cart.reducer'
+
 export const CartMenu = ({show, setShow}) => {
 const items = useSelector(state=>state.cart.items) ?? []
+const dispatch = useDispatch()
 // const total_price = useMemo(()=>items.reduce((price, item)=>price + (item.count*item.price), 0) ,items) 
 const total_price = useSelector((state)=>state.cart.items.reduce
 ((price, item) => price + (item.price*item.count),0)) 
@@ -31,8 +34,12 @@ count + item.count, 0))
         <div className={styles.cartMenuItem_tittle}>{item.tittle}</div>
         <div className={styles.cartMenuItem_description}>{item.description}</div>
         <div className={styles.cartMenuItem_control}>
-          <div><BiTrash/></div>
-          <div>{item.count}</div>
+        <div onClick={()=>{dispatch(remove(item))}}><BiTrash/></div>
+        <div onClick={()=>dispatch(addtoCart(item))}><BiPlus/></div>
+           <div>{item.count}</div>
+           <div onClick={()=>dispatch(decrease(item))}><BiMinus/></div>
+         
+          {/* <div>{item.count}</div> */}
         </div>
       </div>
       <div className={styles.cartMenuItem_image}>
