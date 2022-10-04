e21ae79a245fe36f39b8fe71f3bbc8236801e092
digicala <narjessheikhali@gmail.com>
2022-10-04 23:55:26 +0330

    amazing single

--- digicala/src/components/WithSpinner/WithSpinner.jsx
+++ digicala/src/components/WithSpinner/WithSpinner.jsx
@@ -66,3 +66,32 @@ const WithSpinner = (WrappedComponent, getData) => {
 // }
 
 export default WithSpinner
+
+export const WithSpinners = (WrappedComponent, getData) => {
+    return function WithSpinner()  {
+        // return function WrappedComponent(){
+        const [isLoading, setLoading] = useState(true)
+        const [error, setError] = useState(false)
+        const [data, setData] = useState(undefined)
+        const { id } = useParams()
+        useEffect(() => {
+            getData(id)
+                .then(data =>{
+                    console.log(data);
+                    setData(data)})
+                .catch(() => {
+                    setError(true)
+                })
+                .finally(() =>{setLoading(false)})
+        }, [id])
+        if (isLoading) {
+            return (<p>...loading</p>)
+        }
+        if (error) {
+            return <p> oh there is an error</p>
+        }
+        else {
+            return <WrappedComponent product={data.product} />
+        }
+    }
+    }
