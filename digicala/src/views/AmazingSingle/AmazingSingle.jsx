0e9f4473b1cad3721f9520f20bad16a5f7ed4edf
digicala <narjessheikhali@gmail.com>
2022-10-05 15:59:38 +0330

    amazingsingle finish

--- digicala/src/views/AmazingSingle/AmazingSingle.jsx
+++ digicala/src/views/AmazingSingle/AmazingSingle.jsx
@@ -2,26 +2,35 @@ import React from 'react'
 import styles from './AmazingSingle.style.module.css'
 import { WithSpinners } from '../../components/WithSpinner/WithSpinner'
 import { getuser } from '../../api/Products.api'
+import { useDispatch } from 'react-redux'
+import { addtoCart } from '../../redux/reducers/Cart.reducer'
 const AmazingSingle = ({product}) => {
+  const dispatch = useDispatch(); 
+  const handleAddtoCart = (user) =>{
+    dispatch(addtoCart(user))
+    // console.log(user+ "userrrrrrrrrrrrrr");
+  }  
   return (
     <div className={styles.AmazingSingle}>
-      <div className='details'>
+      <div className={styles.details}>
       <div className={styles.big_img}>
       <img src={product.pic} alt=""/>  
       </div>         
       <div className={styles.box}>
       <div className={styles.row}>
           <h2>{product.tittle}</h2>
-          <span>{product.price}</span>
-        </div>
         <p>{product.description}</p>
         <p>{product.content}</p>
-        <div className={styles.thumb}>
+        <span>{product.price}</span>
+
+        </div>
+       
+        {/* <div className={styles.thumb}>
           <img 
           src={product.pic} 
           alt=""/>       
-        </div>
-        <button className={styles.cart}>Add to cart</button>
+        </div> */}
+        <button onClick={(()=>handleAddtoCart(product))}  className={styles.cart}>Add to cart</button>
         </div>   
       </div>
       
